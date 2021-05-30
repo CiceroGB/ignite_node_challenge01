7c061c13fabb56ba88021b0751ccdfa723d4724b
Cicero Gasparini Brunelli <brunelli.cg@gmail.com>
2021-05-30 19:21:17 -0300

    create

--- src/index.js
+++ src/index.js
@@ -1,25 +1,50 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const user = users.find(user => user.username === username);
+
+  if (!user) {
+    return res.status(400).json({ error: "User not found!" })
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = request.body;
+  const userAlreadyExists = users.some(user => user.username === username)
+
+  if (userAlreadyExists) {
+    return response.status(400).json({ error: "Username already exist" })
+  }
+
+  costumers.push({
+    id: uuidv4(),
+    name,
+    username,
+    todo: []
+  })
+
+  return response.sendStatus(201);
+
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
   // Complete aqui
+  
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
